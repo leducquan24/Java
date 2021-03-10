58d214416bbbe0494696f8b379dba8a714f657c8
leducquan24 <71084011+leducquan24@users.noreply.github.com>
2021-03-10 21:14:25 +0700

    nopbaitap

---
+++ src/lab/s2/lab/Main.java
@@ -0,0 +1,21 @@
+package lab.s2.lab;
+
+public class Main {
+    public static void main(String [] args){
+        SoNguyenTo s = new SoNguyenTo();
+        System.out.printl("SNT: "+s.getA());
+        System.out.println("SNT: "+s.getA());
+        s.timSoNguyenToTiepTheo();
+        System.out.println("SNT: "+s.getA());
+        s.timSoNguyenToTiepTheo();
+        System.out.println("SNT: "+s.getA());
+        s.timSoNguyenToTiepTheo();
+        System.out.println("SNT: "+s.getA());
+        s.timSoNguyenToTiepTheo();
+        System.out.println("SNT: "+s.getA());
+        s.timSoNguyenToTiepTheo();
+        System.out.println("SNT: "+s.getA());
+        s.timSoNguyenToTiepTheo();
+        System.out.println("SNT: "+s.getA());
+    }
+}
