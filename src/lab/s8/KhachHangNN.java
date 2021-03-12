dcc7a85dabed03c5cd73cf1fbeae6d2af007a8a1
leducquan24 <71084011+leducquan24@users.noreply.github.com>
2021-03-12 13:55:51 +0700

    nopbaitap

---
+++ src/lab/s8/KhachHangNN.java
@@ -0,0 +1,17 @@
+package lab.s8;
+
+public class KhachHangNN extends  KhachHang{
+    String country;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    @Override
+    public double tinhTien(){
+        return getSoLuong()*2000;
+    }
+}
